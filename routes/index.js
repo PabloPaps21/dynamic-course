3caf592fc4689c38f5c1331c1e7a5f5b11348807
Edgar Carro <edgc21@hotmail.com>
2019-11-26 21:31:01 -0600

    controller & routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,14 @@ const router  = express.Router();
 
 const Course = require('../models/Course')
 
+const {
+  signupGet,
+  signupPost,
+  loginGet,
+  loginPost,
+  logOut
+} = require("../controllers/auth.controller");
+
 router.get('/', (req, res, next) => res.render('index'))
 
 router.get('/profile', async (req, res, next) => {
